f37ddb06c1bbd9b73da0cd922420847a797e1af9
Himanshu Singh Chouhan <himanshusc8374@gmail.com>
2026-08-08 21:17:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (30.56%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkPal(int l,int r,string &cleaned){
+        if(l>=r) return true;
+        else if(cleaned[l]!=cleaned[r]) return false;
+        l++;
+        r--;
+        return checkPal(l,r,cleaned);
+    }
+    bool isPalindrome(string s){
+        string cleaned ="";
+        for(char ch : s){
+            if (isalnum(ch)){
+            cleaned += tolower(ch);
+            }
+        }
+        int n = cleaned.size();
+        int l =0;
+        int r=n-1;
+        return checkPal(l,r,cleaned);
+    }
+};
