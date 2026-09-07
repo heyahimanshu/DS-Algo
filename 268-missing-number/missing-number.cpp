95885fbc8985679939013c5b13f74023b96a5208
Himanshu Singh Chouhan <himanshusc8374@gmail.com>
2026-09-07 19:31:57 +0530

    Time: 336 ms (5.01%) | Memory: 21.9 MB (38.34%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int i;
+        int n = nums.size();
+        for(i=0;i<=n;i++){
+            int flag =0;
+            for(int j=0;j<n;j++){
+                if(nums[j]==i){
+                    flag=1;
+                    break;
+                } 
+            }
+            if(flag==0) return i;
+        }
+        return -1;
+    }
+};
